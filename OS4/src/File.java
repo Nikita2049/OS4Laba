99044ee1415a1da207b5c54286657dcd2c63c865
Никита Гурин <nikita.gurin2018@yandex.ru>
2020-06-02 08:13:42 +0000

    OS4

---
+++ OS4/src/File.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+
+public class File {
+	private int fileSize;
+	private int fileId;
+	private Knot knots;
+
+	public File(int fileId, int fileSize) {
+		this.fileId = fileId;
+		this.fileSize = fileSize;
+	}
+
+	public Knot getKnot() {
+		return knots.nextKnot();
+	}
+	
+	public int getId() {
+		return fileId;
+	}
+
+	public int fileSize() {
+		return fileSize;
+	}
+}
