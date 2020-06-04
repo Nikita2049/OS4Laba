570a00afcc24cde80c754201fcb5cbc799c65160
Никита Гурин <nikita.gurin2018@yandex.ru>
2020-06-04 12:37:38 +0000

    last_fix

--- OS4/src/File.java
+++ OS4/src/File.java
@@ -2,13 +2,11 @@ import java.util.ArrayList;
 
 public class File {
 	private int fileSize;
-	private int fileId;
 	private ArrayList<Knot> knots;
 	
 	
-	public File(int fileId, int fileSize) {
+	public File(int fileSize) {
 		knots = new ArrayList<Knot>();
-		this.fileId= fileId;
 		this.fileSize = fileSize;
 	}
 	
@@ -18,18 +16,12 @@ public class File {
 	
 	public void addKnot(Knot knot) {
 		knots.add(knot);
-		knot.setFile(fileId);
 	}
 	
 	public void removeKnots() {
 		for (Knot knot : knots) {
-			knot.setFile(-1);
 		}
 		knots.clear();
-	}
-	
-	public int getId() {
-		return fileId;
 	}
 	
 	public int fileSize() {
--- OS4/src/Knot.java
+++ OS4/src/Knot.java
@@ -1,30 +1,20 @@
 public class Knot {
 	private int knotId;
-	private int fileId;
 	private Knot knot;
 
-	public Knot(int fileId, int knotId) {
-		this.fileId = fileId;
+	public Knot(int knotId) {
 		this.knotId = knotId;
-	}
-
-	public void setFile(int fileId) {
-		this.fileId = fileId;
 	}
 
 	public int getKnotId() {
 		return knotId;
 	}
 	
-	public void setKnot(int fileId, int knotId) {
-		knot = new Knot(fileId, knotId);
+	public void setKnot(int knotId) {
+		knot = new Knot(knotId);
 	}
 
 	public Knot nextKnot() {
 		return knot;
-	}
-
-	public int getId() {
-		return fileId;
 	}
 }
